41fe0bb4662c16e18827ce55c4833f144e030bc4
NataliiaChernieva <NataliiaChernieva@gmail.com>
2021-09-14 22:49:30 +0300

    инициализация проекта и настройка

---
+++ contacts.js
@@ -0,0 +1,28 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+const contactsPath = fs.readFile(path:'./db/contacts.json')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// https://prnt.sc/1s76h5a - обработка ошибки
